575eae7230c1b0a1ee3d8e35de6d42cbffb805fa
Emily Schaumann <42627450+eschaumann@users.noreply.github.com>
2019-01-30 09:53:32 -0700

    add sentence, fix mistake

--- tests.sh
+++ tests.sh
@@ -1,9 +1,14 @@
 
-	declare -a sentences=("gato")
+declare -a sentences=(	"gato"
+			"ver"
+			"veo"
+			"gatos"
+			"vemos gatos")
 
 		
 
-	for sent in "${sentences[@]}"
-	do
+for sent in "${sentences[@]}"
+do
 		echo -e ${sent}$"\t\t"$(echo ${sent} | lt-proc esp-eng.automorf.bin |   gawk 'BEGIN{RS="$"; FS="/";}{nf=split($1,COMPONENTS,"^"); for(i = 1; i<nf; i++) printf COMPONENTS[i]; if($2 != "") printf("^%s$",$2);}' |   apertium-transfer apertium-esp-eng.esp-eng.t1x esp-eng.t1x.bin esp-eng.autobil.bin  |   lt-proc -g esp-eng.autogen.bin)
+done
 
